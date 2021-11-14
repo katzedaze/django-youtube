71d34f36a241fe7b3b28626a2831a1568a72ad18
katzedaze <fairycat.caitsith@gmail.com>
2021-11-14 15:31:02 +0900

    デプロイ設定

--- youtube/settings.py
+++ youtube/settings.py
@@ -155,8 +155,13 @@ except ImportError:
 # ローカル用設定
 if DEBUG:
     ALLOWED_HOSTS = ['*']
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    import environ
+    env = environ.Env()
+    env.read_env(os.path.join(BASE_DIR, '.env'))
+
+    YOUTUBE_API_KEY = env('YOUTUBE_API_KEY')
 
 if not DEBUG:
     import environ
@@ -164,16 +169,15 @@ if not DEBUG:
     env.read_env(os.path.join(BASE_DIR, '.env'))
 
     YOUTUBE_API_KEY = env('YOUTUBE_API_KEY')
-
     SECRET_KEY = env('SECRET_KEY')
     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_TLS = True
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    # EMAIL_HOST = 'smtp.gmail.com'
+    # EMAIL_PORT = 587
+    # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    # EMAIL_USE_TLS = True
 
     STATIC_ROOT = '/usr/share/nginx/html/static'
     MEDIA_ROOT = '/usr/share/nginx/html/media'
