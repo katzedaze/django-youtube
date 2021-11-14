b40ff624df2755a67862ac27092873abfd8f01ce
katzedaze <fairycat.caitsith@gmail.com>
2021-11-14 14:43:55 +0900

    fixed commit

--- youtube/settings.py
+++ youtube/settings.py
@@ -11,28 +11,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from pathlib import Path
 
-import django_heroku
-import environ
+# from pathlib import Path
+
+# import django_heroku
+# import environ
 
 # from app.views import YOUTUBE_API
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#-0j(&^j1k&qgzmy1wg8l+c1h1$@^n3+18#31((9yu+#q*ura'
+# SECRET_KEY =
+# 'django-insecure-0#-0j(&^j1k&qgzmy1wg8l+c1h1$@^n3+18#31((9yu+#q*ura'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -84,7 +85,8 @@ WSGI_APPLICATION = 'youtube.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -126,17 +128,52 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Youtube API
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
+# env = environ.Env()
+# env.read_env(os.path.join(BASE_DIR, '.env'))
 
-YOUTUBE_API_KEY = env('YOUTUBE_API_KEY')
+# YOUTUBE_API_KEY = env('YOUTUBE_API_KEY')
 
 # Heroku
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
+
+# デプロイ設定
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+# ローカル用設定
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:
+    import environ
+    env = environ.Env()
+    env.read_env(os.path.join(BASE_DIR, '.env'))
+
+    YOUTUBE_API_KEY = env('YOUTUBE_API_KEY')
+
+    SECRET_KEY = env('SECRET_KEY')
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = True
+
+    STATIC_ROOT = '/usr/share/nginx/html/static'
+    MEDIA_ROOT = '/usr/share/nginx/html/media'
